bb7a0810b60c547278e9b9b45b8bc8af0b3bcb3d
Alejandro Norberto Pasquini <alejandronp9@gmail.com>
2016-04-28 21:04:29 -0300

    d

---
+++ installVSCode.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+dest="/opt"
+tmpDest="/tmp/vscode"
+folderInstaller='VSCode'
+shortcut='/usr/share/applications/vscode.desktop'
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+elif [  -d $dest/$folderInstaller ]; then
+    echo "Visual Studio Code is Installed. ¿Remove it? y/c"
+    read r
+    if [ "$r"  == "y" ]; then 
+	rm -R -f $dest/$folderInstaller
+	rm -f $shortcut
+    else
+    echo "Not removed"
+    fi    
+echo "script is finished"
+exit
+fi
+
+mkdir $tmpDest
+mkdir $dest
+
+echo "Ingrese el zip de origen"
+read origin
+origin=$(echo $origin | tr -d "'")
+echo $origin 
+unzip   $origin  -d $tmpDest/
+tmpFolderInstaller=$(ls $tmpDest)
+mv $tmpDest/$tmpFolderInstaller $dest/$folderInstaller
+rm -R -f $tmpDest
+
+echo "[Desktop Entry]
+Comment=Microsoft Visual Studio Code
+Terminal=false
+Name=Visual Studio Code
+Icon="$dest/$folderInstaller"/resources/app/resources/linux/vscode.png
+Exec="$dest/$folderInstaller"/Code
+Type=Application" >> $shortcut
+
